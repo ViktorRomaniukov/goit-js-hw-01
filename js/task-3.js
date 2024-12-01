47f76b61f60878788de2918c433da0708662f1bf
Viktor_Romaniukov <viktorrvi@email.ua>
2024-12-01 11:02:15 +0200

    add hw

--- js/task-3.js
+++ js/task-3.js
@@ -6,6 +6,6 @@ function getElementWidth(content, padding, border) {
   return borderBox;
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(`Загальна ширина елемента складає: ${getElementWidth('50px', '8px', '4px')}px.`);
+console.log(`Загальна ширина елемента складає: ${getElementWidth('60px', '12px', '8.5px')}px.`);
+console.log(`Загальна ширина елемента складає: ${getElementWidth('200px', '0px', '0px')}px.`);
